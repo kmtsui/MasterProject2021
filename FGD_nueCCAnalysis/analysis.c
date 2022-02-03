251cbcd933b3f61e851a59035223fa4ab8104b89
Ka Ming Tsui <K.M.Tsui@liverpool.ac.uk>
2022-02-03 15:49:35 +0000

    Update after meeting

--- FGD_nueCCAnalysis/analysis.c
+++ FGD_nueCCAnalysis/analysis.c
@@ -7,7 +7,7 @@ void analysis()
 
     // Alternatively use a TChain object to load a set of files
     TChain* in_chain = new TChain("default"); // initiate with the name of the TTree to be processed
-    in_chain->Add("data/FHC/nue/run8a_0*.root"); // add files, wildcards work
+    in_chain->Add("data/FHC/nue/run8*.root"); // add files, wildcards work
 
     std::cout<<"TFile Entries = "<<t->GetEntries()<<std::endl;
     std::cout<<"TChain Entries = "<<in_chain->GetEntries()<<std::endl;
@@ -15,20 +15,39 @@ void analysis()
     // Define variables and assign them to the corresponding branches, same commands for TTree and TChain
     // Use TTree::Print() to list all available branches and data types
     int nue_signal;
+    int nuesimpletopo;
     int accum_level[100]; // for array variables, reserve a large enough size
     float selelec_mom[100];
-    t->SetBranchAddress("nue_signal",&nue_signal);
-    t->SetBranchAddress("accum_level",accum_level);
-    t->SetBranchAddress("selelec_mom",selelec_mom);
+    int nEntries;
 
+    // Uncomment if using the single TTree
+    // t->SetBranchAddress("nue_signal",&nue_signal);
+    // t->SetBranchAddress("accum_level",accum_level);
+    // t->SetBranchAddress("selelec_mom",selelec_mom);
+    // t->SetBranchAddress("nuesimpletopo",&nuesimpletopo);
+    // nEntries = t->GetEntries();
+
+    // Uncomment if using the TChain
+    in_chain->SetBranchAddress("nue_signal",&nue_signal);
+    in_chain->SetBranchAddress("nuesimpletopo",&nuesimpletopo);
+    in_chain->SetBranchAddress("accum_level",accum_level);
+    in_chain->SetBranchAddress("selelec_mom",selelec_mom);
+    nEntries = in_chain->GetEntries();
+	
     // Initialize histogram for plotting
+    // For stack histograms one may use THStack
     TH1F* hist_mom = new TH1F("hist_mom","hist_mom",18,0,5400);
-    for (int i=0; i<t->GetEntries();i++)
+    for (int i=0; i<nEntries;i++)
     {
-        t->GetEntry(i); // loads all variables that have been connected to branches
+	    if (i%100000==0) std::cout<<"Processing "<<i<<" out of "<<nEntries<<std::endl;
+
+        // Choose the correct input 
+        // t->GetEntry(i); // loads all variables that have been connected to branches
+	    in_chain->GetEntry(i);
 
         // Fill histogram if an event meets a certain condition
-        if (nue_signal==1 && accum_level[0]>17)
+        //if ((nuesimpletopo==1 || nuesimpletopo==2) && accum_level[0]>17)
+        if ((nuesimpletopo==3) && accum_level[0]>17)
         {
             hist_mom->Fill(selelec_mom[0]);
         }
--- README.md
+++ README.md
@@ -6,8 +6,18 @@ Sample MC data for the nue cross-section analysis in https://link.springer.com/a
 ### ref
 Partial source code of event selection and variables
 
+nueCCSelection/antiNueCCSelection: selection cuts and step numbers
+
+nueCCAnalysis: variable definition
+
+CategToolsNueCC: definition of signal and background topology
+
 ### data
 Data files for neutrino mode (FHC) and antineutrino mode (RHC) run
 
 ### analysis.c
 Sample analysis code to read data and make plots
+#### Important variables
+accum_level: accumulated number of cuts that an event passes
+
+nuesimpletopo: 1 & 2 = nueCC, 3 = gamma bkg, 4 = muon bkg, others = others
